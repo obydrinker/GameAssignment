d5ce681ffd0aecd931b55100842a9d5adbb8e124
Noel Mortensen <noel.mortensen@elev.ga.lbs.se>
2018-10-09 10:29:45 +0200

    Fixade ALLT... förutom S nivå...

--- Assets/Spaceship1.cs
+++ Assets/Spaceship1.cs
@@ -7,9 +7,7 @@ public class Spaceship1 : MonoBehaviour
     //variables
     public int spaceshipSpeed;
     [Range(-300, 300)]
-    public float spaceshipRotationSpeed1;//higher rotation speed.
-    [Range(-200, 200)]
-    public float spaceshipRotationSpeed2;//less rotation speed.
+    int spaceshipRotationSpeed;
     public SpriteRenderer spaceshipBodyColor;
     public Color newcolor;
     public float timer;
@@ -19,6 +17,13 @@ public class Spaceship1 : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //randomize ship speed
+        spaceshipSpeed = Random.Range(5, 21);
+        //randomize ship rotation speed
+        spaceshipRotationSpeed = Random.Range(200, 301);
+        //randomize the spawnpoint of the ship.
+        transform.position = new Vector3(Random.Range(-10f, 11f), Random.Range(-5, 6f));
+
     }
 
     // Update is called once per frame
@@ -28,15 +33,15 @@ public class Spaceship1 : MonoBehaviour
         transform.Translate(0, spaceshipSpeed * Time.deltaTime, 0, Space.Self);
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0f, 0f, -spaceshipRotationSpeed1 * Time.deltaTime);
+            transform.Rotate(0f, 0f, -spaceshipRotationSpeed * Time.deltaTime);
             spaceshipBodyColor.color = new Color(0f, 0f, 1f);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f, 0f, spaceshipRotationSpeed2 * Time.deltaTime);
+            transform.Rotate(0f, 0f, (spaceshipRotationSpeed-100) * Time.deltaTime);
             spaceshipBodyColor.color = new Color(0f, 1f, 0f);
         }
-        //code for slowing down the spaceship speed by half if holding S.
+        //code for slowing down the spaceship speed by half if holding S and returning to normal speed if not holding S.
         if (Input.GetKeyDown(KeyCode.S))
         {
             spaceshipSpeed = spaceshipSpeed / 2;
@@ -57,6 +62,30 @@ public class Spaceship1 : MonoBehaviour
         }
         //Checking when the timer is going to print.
         timerPrint = timerPrint + 1 * Time.deltaTime;
+
+        //code for random color if pressing space.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceshipBodyColor.color = new Color(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f));
+        }
+        //code for screenwrap.'
+        if (transform.position.x <= -10.4)
+        {
+            transform.position = new Vector3(10.3f, transform.position.y);
+        }
+        if (transform.position.x >= 10.4)
+        {
+            transform.position = new Vector3(-10.3f, transform.position.y);
+
+        }
+        if (transform.position.y <= -6.5)
+        {
+            transform.position = new Vector3(transform.position.x, 6.4f);
+        }
+        if (transform.position.y >= 6.5)
+        {
+            transform.position = new Vector3(transform.position.x, -6.4f);
+        }
 
 
 
